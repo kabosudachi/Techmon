9d1ab9a52fcb1f80bb264e511fb0c1b915d01a84
西村真奈 <kabosudachi86@gmail.com>
2015-04-12 19:41:16 +0900

    Initial Commit

---
+++ TechMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechMonster
+//
+//  Created by 西村真奈 on 2015/04/12.
+//  Copyright (c) 2015年 ManaNishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
